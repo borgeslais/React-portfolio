833eef5a1059c47ba37e3e78bee06f3853be402a
borgeslais <borgescrlais@gmail.com>
2024-11-04 17:15:13 -0300

    estilizando sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,8 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/borgeslais.png"></img>
-    <Titulo fontSize={20}>Lais Borges</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lais Borges</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        borgeslais
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedora front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
